6c046f7c768f219a44b345bee5fb582896e73469
Jairo <146075055+Jeremy-buendia@users.noreply.github.com>
2025-06-08 14:13:53 +0200

    change in pdf visor mobile

--- frontend/src/components/main/ventas/detalleVenta.jsx
+++ frontend/src/components/main/ventas/detalleVenta.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
+import { RxCross2 } from "react-icons/rx";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.mjs",
@@ -10,24 +11,42 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 
 export default function PanelDetalleVenta({ venta }) {
   const [numPages, setNumPages] = useState(null);
+  const [rutaPDF, setRutaPdf] = useState(null);
 
   const handleLoadSuccess = ({ numPages }) => {
     setNumPages(numPages);
   };
 
-  let rutaPDF = "";
-  if (venta?.url_ticket) {
-    rutaPDF = `${import.meta.env.VITE_IMG_HOST}${venta?.url_ticket}`;
-  }
+  // let rutaPDF = "";
+  useEffect(() => {
+    if (venta?.url_ticket) {
+      setRutaPdf(`${import.meta.env.VITE_IMG_HOST}${venta.url_ticket}`);
+    } else {
+      setRutaPdf(null);
+      setNumPages(null);
+    }
+  }, [venta]);
 
   return (
-    <div className="h-full overflow-auto p-4">
-      <div className="flex justify-center">
-        <Document file={rutaPDF} onLoadSuccess={handleLoadSuccess}>
-          {Array.from(new Array(numPages), (el, index) => (
-            <Page key={`page_${index + 1}`} pageNumber={index + 1} />
-          ))}
-        </Document>
+    <div>
+      {numPages ? <div className="visorPDF-back"></div> : null}
+      <div className={`h-full overflow-auto p-4 ${numPages ? "visorPDF" : ""}`}>
+        <div className="flex justify-center items-start">
+          <Document file={rutaPDF} onLoadSuccess={handleLoadSuccess}>
+            {Array.from(new Array(numPages), (el, index) => (
+              <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+            ))}
+          </Document>
+          <button
+            onClick={() => {
+              setNumPages(null);
+              setRutaPdf(null);
+            }}
+            className="visorPDF-close-btn"
+          >
+            <RxCross2 />
+          </button>
+        </div>
       </div>
     </div>
   );
